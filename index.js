7e90727663e87946f9ad089621dd55d5638efb6c
Hugo Brito <97696808+hugommbrito@users.noreply.github.com>
2022-09-30 09:21:24 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const products =  [
+    {
+    title: "Lasanha",
+    category: "massas",
+    price: 20.00
+ },
+ {
+    title: "Pizza",
+    category: "massas",
+    price: 45.00
+ },
+ {
+    title: "Chocolate Quente",
+    category: "bebidas",
+    price: 11.00
+ },
+ {
+    title: "Café expresso",
+    category: "bebidas",
+    price: 8.00
+ },
+ {
+    title: "Camarão Internacional",
+    category: "massas",
+    price: 90.00
+ }
+]
+
+const orderFromSmallestToLargestPrice = (array = []) => {
+}
+const orderLargestFromToSmallestPrice = (array = []) => {
+}
+
+
+// console.log("data", orderFromSmallestToLargestPrice(products))
+// console.log("data", orderLargestFromToSmallestPrice(products))
